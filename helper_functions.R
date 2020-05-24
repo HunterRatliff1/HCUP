1cf251a7e7bbc20df8cf6567428cc5b5679f50b7
Hunter Ratliff <Hunterratliff1@gmail.com>
2020-05-24 11:45:03 -0500

    Create helper_functions.R

---
+++ helper_functions.R
@@ -0,0 +1,57 @@
+expand_icd10 <- function(single_code, ..., verbose=T){
+  ### ---------------------- DESCRIPTION ---------------------- ###
+  ###   Function that given ICD-10-CM codes, will return a table
+  ###   with their descriptions. Additionally, this expands any 
+  ###   children codes if the ICD-10 code ends in 'x'.
+  ###
+  ###   NOTE: this will not work ICD-10-PCS codes (procedure codes)
+  ###         or with ICD-9 codes
+  ###
+  ###   This only handles single codes, but can be easily mapped
+  ###   using purrr:
+  ###
+  ###   > c("N25.0x", "Z49.x", "Z99.2x") %>% 
+  ###   +     map_dfr(expand_icd10, Comorbidity = "Dialysis")
+  
+  # Required packages
+  library(icd)
+  library(tidyverse)
+  
+  dots <- list(...)
+  
+  # Should be a single code
+  testthat::expect_length(single_code, 1)
+  
+  # Logical flag if children should be returned
+  get_children <- str_detect(single_code, "x$")
+  
+  # If present, remove the x and make short code
+  parent <- as.icd10(str_remove(single_code, "x$")) %>% decimal_to_short()
+  
+  # Give warning if code not found
+  if(!is_defined(parent)) warning("parent code `",parent, "` not defined ICD-10-CM code")
+  
+  
+  # New object called target codes
+  target_codes <- parent
+  
+  if(get_children){
+    target_codes <- parent %>% children()
+    if(length(target_codes)>1 & verbose==T) message(length(target_codes)-1,
+                                                    " children codes found for ", single_code)
+  }
+  
+  
+  # Make the table
+  df <- target_codes %>%
+    explain_table() %>% as_tibble() %>%
+    mutate(code_decimal = short_to_decimal(code),
+           parent_code  = single_code) %>%
+    select(parent_code, ICD10=code, code_decimal, short_desc, long_desc) %>%
+    mutate_all(as.character) %>%
+    
+    mutate(...)  # forward tidy dots
+  
+  df %>% select(names(dots), parent_code:long_desc) # reorder
+  
+}
